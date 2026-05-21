7888a248e265da310c8318b3ba173473ed6cfbf2
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-05-21 17:52:03 +0500

    initial commit: setup NestJS SaaS project

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import {NestFactory,Reflector} from '@nestjs/core';
+import {AppModule } from './app.module';
+import {ClassSerializerInterceptor, ValidationPipe} from '@nestjs/common';
+import {AllExceptionsFilter} from './common/filters/AllExceptionsFilter';
+import {HttpExceptionFilter} from './common/filters/HttpExceptionFilter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+async function bootstrap(){
+    const app=await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist:true,
+            forbidNonWhitelisted:true,
+            transform:true,
+        }),
+    );
+
+    app.useGlobalInterceptors(
+        new ClassSerializerInterceptor(app.get(Reflector)),
+        new ResponseInterceptor(),
+    );
+
+    app.useGlobalFilters(
+        new HttpExceptionFilter(),
+        new AllExceptionsFilter(),
+    );
+
+    await app.listen(3000);
+    console.log("http://localhost:3000");
+
+    bootstrap();
+}
