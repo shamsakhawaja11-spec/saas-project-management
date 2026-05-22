c377f6c309c16ad8bc20cad3ea915c27f1cd5402
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-05-22 18:58:54 +0500

    Created Entities

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { Exclude } from "class-transformer";
+import { Column,Entity,PrimaryGeneratedColumn,CreateDateColumn, UpdateDateColumn } from "typeorm";
+@Entity('users')
+export class UserEntity{
+    @PrimaryGeneratedColumn('uuid')
+    id:string;
+
+    @Column({type:"varchar",length:200})
+    name:string;
+
+    @Column({type:"varchar",unique:true,length:120})
+    email:string;
+
+    @Exclude()
+    @Column({type:"varchar",length:100})
+    password:string;
+
+
+    @CreateDateColumn()
+    createdAt:Date;
+
+    @UpdateDateColumn()
+    updatedAt:Date;
+
+
+}
