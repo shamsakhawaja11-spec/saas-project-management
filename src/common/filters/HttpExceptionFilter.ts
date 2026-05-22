d5cb08f87cd614de06839985913c7b7a05f81654
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-05-22 16:06:41 +0500

    feat: create HTTP exception filter for formatting error responses

---
+++ src/common/filters/HttpExceptionFilter.ts
@@ -0,0 +1,31 @@
+import {ExceptionFilter,Catch,ArgumentsHost,HttpException,Logger} from '@nestjs/common'
+import { Request,Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter{
+    private readonly logger=new Logger(HttpExceptionFilter.name);
+    catch(exception:HttpException,host:ArgumentsHost){
+        const ctx=host.switchToHttp();
+        const response=ctx.getResponse<Response>();
+        const request=ctx.getRequest<Request>();
+        const status=exception.getStatus();
+        const exceptionResponse=exception.getResponse();
+
+        let message='internal server error';
+        let errors=null;
+        if(typeof exceptionResponse==='object'){
+            message=exceptionResponse['message']||message;
+            errors=exceptionResponse['error']||null;
+        }
+        const errorResponse={
+            statusCode:status,
+            message,
+            errors,
+            timestamp:new Date().toISOString(),
+            path:request.url,
+        };
+        this.logger.error(`${request.method} ${request.url}`,JSON.stringify(errorResponse));
+
+        response.status(status).json(errorResponse);
+    }
+}
