893a210e94a273f9d9ce3f9eab996f2f19ad8666
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-05-24 01:02:55 +0500

    feat: Create Workspace entity with owner relationship

---
+++ src/workspaces/entities/workspace.entity.ts
@@ -0,0 +1,21 @@
+import { IsString } from "class-validator";
+import { Entity,Column, CreateDateColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { User } from "../../users/entities/user.entity";
+
+@Entity('workspaces')
+export class Workspace{
+    @PrimaryGeneratedColumn('uuid')
+    id!:string;
+    @Column({type:"varchar",length:100})
+    name!:string;
+    @Column({type:"varchar",length:50, nullable:true})
+    description!:string;
+    @ManyToOne(()=>User)
+    owner!:User;
+    @Column({type:'uuid'})
+    ownerId!:string;
+    @CreateDateColumn()
+    createdAt!:Date;
+    @UpdateDateColumn()
+    updatedAt!:Date;
+}
