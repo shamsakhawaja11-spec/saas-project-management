e239ad035d39ab7caf0951cf08f9b8e3ec2f4dfa
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-05-25 23:19:52 +0500

    Created Project Entity

---
+++ src/projects/entities/project.entity.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Workspace } from "../../workspaces/entities/workspace.entity";
+
+@Entity('projects')
+export class Project{
+    @PrimaryGeneratedColumn('uuid')
+    id!:string;
+
+    @Column({length:100})
+    name!:string;
+
+    @Column({type:"varchar",length:123,nullable:true})
+    description!:string;
+
+    @ManyToOne(()=>Workspace)
+    @JoinColumn({name:'workspaceId'})
+    workspace!:Workspace;
+
+    @Column({type:'uuid'})
+    workspaceId!:string;
+
+    @CreateDateColumn()
+    createdAt!:Date;
+    
+    @UpdateDateColumn()
+    updatedAt!:Date;
+}
--- src/workspaces/controllers/workspaces.controller.ts
+++ src/workspaces/controllers/workspaces.controller.ts
@@ -8,34 +8,29 @@ export class WorkspacesController{
 
     @Post()
     async createWorkspace(@Body() createWorkspaceDto:CreateWorkspaceDto, @Request() req:any):Promise<WorkspaceResponseDto>{
-        const id='36802505-18ea-4026-8c0d-5c6aa01b9d29';
+        const id=req.user?.id;
         return this.workspacesService.createWorkspace(id,createWorkspaceDto);
     }
     @Get()
     async getWorkspaces(@Request() req:any):Promise<WorkspaceResponseDto[]>{
-        const id='36802505-18ea-4026-8c0d-5c6aa01b9d29';
 
-        //const id=req.user?.id;
+        const id=req.user?.id;
         return this.workspacesService.getWorkspacesByUserId(id);
     }
     @Get(':id')
     async getworkspaceById(@Param('id') id:string,@Request() req:any){
-       // const ownerId=req.user?.id;
-        const ownerId='36802505-18ea-4026-8c0d-5c6aa01b9d29';
+        const ownerId=req.user?.id;
         return this.workspacesService.getWorkspaceById(id,ownerId);
     }
     @Put(':id')
     async updateWorkspace( @Param('id') id:string,@Body() updateWorkspace:Partial<CreateWorkspaceDto>):Promise<WorkspaceResponseDto>{
-       // const ownerId=req.user?.id;
-        const ownerId='36802505-18ea-4026-8c0d-5c6aa01b9d29';
+       const ownerId=req.user?.id;
         return this.workspacesService.updateWorkspace(id,ownerId,updateWorkspace);
     }
     @Delete(':id')
     async deleteWorkspace(@Param('id') id:string,@Request() req:any):Promise<void>{
-      //  const ownerId=req.user?.id;
-        const ownerId='36802505-18ea-4026-8c0d-5c6aa01b9d29';
+        const ownerId=req.user?.id;
 
         return this.workspacesService.deleteWorkspace(id,ownerId);
     }
-
 }
