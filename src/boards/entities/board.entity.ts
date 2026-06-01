8da8f855a57a2cff60299b6f19823b6987cc29a0
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-01 19:53:08 +0500

    feat: add board entity

---
+++ src/boards/entities/board.entity.ts
@@ -0,0 +1,21 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Project } from "../../projects/entities/project.entity";
+
+@Entity('boards')
+export class Board{
+    @PrimaryGeneratedColumn('uuid')
+    id!:string;
+    @Column({length:100})
+    name!:string;
+    @Column({type:'varchar',length:100,nullable:true})
+    description!:string;
+    @ManyToOne(()=>Project,(project)=>project.boards)
+    @JoinColumn({name:'projectId'})
+    project!:Project;
+    @Column({type:'uuid'})
+    projectId!:string
+    @CreateDateColumn()
+    createdAt!:Date;
+    @UpdateDateColumn()
+    updatedAt!:Date;
+}
