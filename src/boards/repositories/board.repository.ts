8d87adec3cf484222323adfd366cd2c1a0a6f7b6
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-03 00:09:59 +0500

    feat: add boards repository

---
+++ src/boards/repositories/board.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { Board } from "../entities/board.entity";
+import { DataSource } from "typeorm";
+
+@Injectable()
+export class BoardRepository extends Repository<Board>{
+    constructor(dataSource:DataSource){
+        super(Board,dataSource.createEntityManager());
+    }
+    async findByProjectById(projectId:string):Promise<Board []>{
+        return this.find({where:{projectId},
+            order:{createdAt:'DESC'},
+        });
+    }
+    async findByIdAndProjectId(id:string,projectId:string):Promise<Board | null>{
+        return this.findOne({
+            where:{id,projectId},
+        });
+    }
+    
+}
