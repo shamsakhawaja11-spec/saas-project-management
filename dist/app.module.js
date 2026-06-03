f7b18be318ff1bbfc727539d8b39896cf520ff66
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-03 16:03:03 +0500

    feat: complete boards module - all endpoints tested

--- dist/app.module.js
+++ dist/app.module.js
@@ -14,6 +14,7 @@ const users_module_1 = require("./users/users.module");
 const auth_module_1 = require("./auth/auth.module");
 const workspaces_module_1 = require("./workspaces/workspaces.module");
 const projects_module_1 = require("./projects/projects.module");
+const boards_module_1 = require("./boards/boards.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
@@ -22,7 +23,7 @@ exports.AppModule = AppModule = __decorate([
         imports: [
             typeorm_1.TypeOrmModule.forRoot(database_config_1.databaseConfig),
             users_module_1.UsersModule, workspaces_module_1.WorkspacesModule,
-            auth_module_1.AuthModule, projects_module_1.ProjectsModule
+            auth_module_1.AuthModule, projects_module_1.ProjectsModule, boards_module_1.BoardsModule
         ],
     })
 ], AppModule);
--- src/boards/dto/board-response.dto.ts
+++ src/boards/dto/board-response.dto.ts
@@ -4,5 +4,5 @@ export class ResponseBoardDto{
     description?:string;
     projectId!:string;
     createdAt!:Date;
-    updatedAt!:string;
+    updatedAt!:Date;
 }
