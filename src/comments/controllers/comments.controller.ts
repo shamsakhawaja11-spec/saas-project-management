b87c4cc92c97510051affd1d271cd472a71b59fa
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-05 23:26:52 +0500

    feat: add comments controller

---
+++ src/comments/controllers/comments.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Request, UseGuards } from "@nestjs/common";
+import { CommentsService } from "../services/comments.service";
+import { CreateCommentDto, UpdateCommentDto, CommentResponseDto } from "../dto";
+import { JwtAuthGuard } from "../../auth/guards/jwt-auth.guard";
+
+@UseGuards(JwtAuthGuard)
+@Controller('comments')
+export class CommentsController {
+  constructor(private commentsService: CommentsService) {}
+
+  @Post()
+  async create(@Request() req: any, @Body() createCommentDto: CreateCommentDto): Promise<CommentResponseDto> {
+    return this.commentsService.create(req.user.id, createCommentDto);
+  }
+
+  @Get('task/:taskId')
+  async findAllByTask(@Param('taskId') taskId: string): Promise<CommentResponseDto[]> {
+    return this.commentsService.findAllByTask(taskId);
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<CommentResponseDto> {
+    return this.commentsService.findById(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto, @Request() req: any): Promise<CommentResponseDto> {
+    return this.commentsService.update(id, updateCommentDto, req.user.id);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string, @Request() req: any): Promise<void> {
+    await this.commentsService.remove(id, req.user.id);
+  }
+}
