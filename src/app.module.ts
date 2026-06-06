b4ec049b7104e9a847d6733be8f37561b4a338b0
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-06 23:36:13 +0500

    feat: register time tracking module in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,14 @@ import { ProjectsModule } from './projects/projects.module';
 import { BoardsModule } from './boards/boards.module';
 import { TasksModule } from './tasks/tasks.module';
 import { CommentsModule } from './comments/comments.module';
+import { TimeTrackingModule } from './time-tracking/time-tracking.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(databaseConfig),
     UsersModule,WorkspacesModule,
     AuthModule,ProjectsModule,BoardsModule,
-    TasksModule,CommentsModule,
+    TasksModule,CommentsModule,TimeTrackingModule,
   ],
 })
 export class AppModule {}
