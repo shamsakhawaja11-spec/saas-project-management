23585f7edab4fb85cf5b75a32f72ff87460bbdac
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-07 11:41:23 +0500

    feat: add create notification DTO

---
+++ src/notifications/dto/create-notification.dto.ts
@@ -0,0 +1,16 @@
+import { IsEAN, IsEnum, IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator";
+import { NotificationType } from "../entities/notification.entity";
+
+export class CreateNotificationDto{
+    @IsEnum(NotificationType)
+    @IsNotEmpty()
+    type!:NotificationType;
+    @IsString()
+    @IsNotEmpty()
+    message!:String;
+    @IsUUID()
+    @IsNotEmpty()
+    userId!:string;
+    @IsOptional()
+    metaData?:Record<string,any>;
+}
