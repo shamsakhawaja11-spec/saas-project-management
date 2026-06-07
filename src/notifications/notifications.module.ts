2572a4ebb45c8b25a91a8f8d9894351466c59f41
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-07 16:48:45 +0500

    feat: register gateway and JwtModule in notifications module

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -4,11 +4,18 @@ import { Notification } from "./entities/notification.entity";
 import { NotificationsRepository } from "./repositories/notifications.repository";
 import { NotificationsService } from "./services/notifications.service";
 import { NotificationsController } from "./controllers/notifications.controller";
+import { NotificationsGateway } from "./gateways/notifications.gateway";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-    imports:[TypeOrmModule.forFeature([Notification])],
+    imports:[TypeOrmModule.forFeature([Notification]),
+    JwtModule.register({
+        secret:'meandme',
+        signOptions:{expiresIn:'7d'},
+    }),
+    ],
     controllers:[NotificationsController],
-    providers:[NotificationsRepository,NotificationsService],
+    providers:[NotificationsRepository,NotificationsService,NotificationsGateway],
     exports:[NotificationsService],
 })
 export class NotificationsModule{}
