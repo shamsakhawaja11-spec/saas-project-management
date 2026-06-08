5f4bd5ca9ab5dad82b67ca26b8d3ee28e21c78d0
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-08 12:46:07 +0500

    refactor: use environment variables in database config

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,5 +1,5 @@
-import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
-import {User} from '../users/entities/user.entity';
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { User } from '../users/entities/user.entity';
 import { Workspace } from "../workspaces/entities/workspace.entity";
 import { Project } from "../projects/entities/project.entity";
 import { Board } from "../boards/entities/board.entity";
@@ -7,16 +7,14 @@ import { Task } from "../tasks/entities/task.entity";
 import { Comment } from "../comments/entities/comment.entity";
 import { TimeEntry } from "../time-tracking/entities/time-entries.entity";
 import { Notification } from "../notifications/entities/notification.entity";
-
-export const databaseConfig:TypeOrmModuleOptions={
-    type:'postgres',
-    host:'localhost',
-    port:5432,
-    username:'postgres',
-    password:'1234',
-    database:'saas_db',
-    entities:[User,Workspace,Project,Board,Task,Comment,TimeEntry,Notification],
-    synchronize:false,
-    logging:true,
-}
-
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || '1234',
+  database: process.env.DB_NAME || 'saas_db',
+  entities: [User, Workspace, Project, Board, Task, Comment, TimeEntry, Notification],
+  synchronize: false,
+  logging: true,
+};
