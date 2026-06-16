8db1ece22134bfaf8feeddcc494c985c3f11a7b5
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-06-16 13:19:19 +0500

    fix: use req.user.sub in notifications controller

--- src/notifications/controllers/notifications.controller.ts
+++ src/notifications/controllers/notifications.controller.ts
@@ -7,23 +7,23 @@ export class NotificationsController{
     constructor(private notificationsService:NotificationsService){}
     @Get()
     async findAllByUser(@Request()req:any){
-        return this.notificationsService.findAllByUser(req.user.id);
+        return this.notificationsService.findAllByUser(req.user.sub);
     }
     @Get('unread')
     async findUnreadByUser(@Request()req:any){
-        return this.notificationsService.findUnreadByUser(req.user.id);
+        return this.notificationsService.findUnreadByUser(req.user.sub);
     }
     @Get('count')
     async countUnread(@Request()req:any){
-        return this.notificationsService.countUnread(req.user.id);
+        return this.notificationsService.countUnread(req.user.sub);
     }
     @Patch('read-all')
     async markAllAsRead(@Request()req:any){
-        await this.notificationsService.markAllAsRead(req.user.id);
+        await this.notificationsService.markAllAsRead(req.user.sub);
     }
     @Patch(':id/read')
     async markOneAsRead(@Param('id')id:string,@Request()req:any){
-        return this.notificationsService.markOneAsRead(id,req.user.id);
+        return this.notificationsService.markOneAsRead(id,req.user.sub);
     }
    
 
